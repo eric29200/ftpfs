e7db20d30c386116eeeee08374b1dc4cf19c8deb
eric29200 <eric_begot@yahoo.fr>
2022-04-22 14:42:37 +0200

    symbolic link : implement readlink (= copy target link to a user buffer) instead of readlink

--- symlink.c
+++ symlink.c
@@ -1,21 +1,32 @@
 #include "ftpfs.h"
 
 /*
- * Get target link.
+ * Read target link.
  */
-static const char *ftpfs_get_link(struct dentry *dentry, struct inode *inode, struct delayed_call *callback)
+static int ftpfs_readlink(struct dentry *dentry, char __user *buf, int buf_len)
 {
-  /* inode must be a link */
-  if (!S_ISLNK(inode->i_mode))
-    return ERR_PTR(-ENOLINK);
+  struct ftpfs_inode_info *ftpfs_inode = ftpfs_i(dentry->d_inode);
+  int len;
   
-  /* target link is stored in inode cached data */
-  return ftpfs_i(inode)->i_cache.data;
+  /* check link */
+  if (!S_ISLNK(dentry->d_inode->i_mode) || !ftpfs_inode->i_cache.data)
+    return -ENOLINK;
+
+  /* compute link length */
+  len = ftpfs_inode->i_cache.len;
+  if (len > buf_len)
+    len = buf_len;
+
+  /* copy to user buffer */
+  if (copy_to_user(buf, ftpfs_inode->i_cache.data, len))
+    return -EFAULT;
+
+  return len;
 }
 
 /*
  * FTPFS symbolic link inode operations.
  */
 struct inode_operations ftpfs_symlink_iops = {
-  .get_link       = ftpfs_get_link,
+  .readlink       = ftpfs_readlink,
 };
